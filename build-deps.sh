5a1389a93a07143b8955c746c6a380e41bf43f3d
Miles Wu <miles.wu@wu-home.co.uk>
2012-03-26 21:19:04 -0400

    Fix error in build-dep script

--- build-deps.sh
+++ build-deps.sh
@@ -22,6 +22,6 @@ cd flac
 make
 make install
 cd $START/prefix/lib/
-install_name_tool -id @executable_path/../Frameworks/libFLAC.dylib libFLAC.dylib
+install_name_tool -id @executable_path/../Frameworks/libFLAC.8.2.0.dylib libFLAC.8.2.0.dylib
 install_name_tool -change $START/libogg/../prefix/lib/libogg.0.dylib @executable_path/../Frameworks/libogg.0.dylib libFLAC.dylib
 
