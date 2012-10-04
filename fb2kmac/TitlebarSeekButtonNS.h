0418c5e64b024688ab3c7f01b54d344680f1cebb
torque <torque@2>
2012-10-03 18:58:00 -0700

    These files also do things probably.

---
+++ fb2kmac/TitlebarSeekButtonNS.h
@@ -0,0 +1,23 @@
+//
+//  TitlebarSeekButtonNS.h
+//  fb2kmac
+//
+//  Created by Someone on 9/12/12.
+//
+//
+
+#import "TitlebarButtonNS.h"
+
+typedef enum { RWSeekButton, FFSeekButton } SeekButtonDirection;
+
+@interface TitlebarSeekButtonNS : TitlebarButtonNS {
+    NSTimer* delayTimer;
+    NSTimer* seekTimer;
+    SeekButtonDirection _buttonType;
+    BOOL _didSeek;
+}
+
+-(void)setType:(SeekButtonDirection)type;
+-(SeekButtonDirection)getType;
+
+@end
