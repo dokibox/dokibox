ccec2957ea43671b8d235be315b55dcd96a1045c
Miles Wu <miles.wu@wu-home.co.uk>
2013-12-17 13:52:52 +0000

    Remove old commented out code in PlaylistView

--- src/PlaylistView.m
+++ src/PlaylistView.m
@@ -73,7 +73,6 @@
         [_trackTableView setDelegate:self];
         [_trackTableView setDataSource:self];
         [_trackTableView registerForDraggedTypes:[NSArray arrayWithObjects:@"trackFilenames", NSFilenamesPboardType, @"playlistTrackIDs", nil]];
-        //[_trackTableView setHeaderView:nil];
         [_trackTableView setIntercellSpacing:NSMakeSize(0, 0)];
         [_trackTableView setDoubleAction:@selector(doubleClickReceived:)];
         [_trackTableView setAllowsMultipleSelection:YES];
@@ -121,11 +120,6 @@
         // Ensure playing column is always first
         [_trackTableView moveColumn:[_trackTableView columnWithIdentifier:@"playing"] toColumn:0];
         
-
-        /*[_tableView setMaintainContentOffsetAfterReload:TRUE];
-        [_tableView setClipsToBounds:TRUE];
-        [_tableView setPasteboardReceiveDraggingEnabled:TRUE];*/
-        
         _addingQueue = dispatch_queue_create(NULL, NULL);
 
         [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(receivedAddTrackToCurrentPlaylistNotification:) name:@"addTrackToCurrentPlaylist" object:nil];
@@ -139,7 +133,6 @@
 {
     [super resizeSubviewsWithOldSize:oldBoundsSize];
     [[[_playlistTableView tableColumns] objectAtIndex:0] setWidth:[_playlistTableView bounds].size.width];
-    //[[[_trackTableView tableColumns] objectAtIndex:0] setWidth:[_trackTableView bounds].size.width];
 }
 
 - (void)drawRect:(NSRect)dirtyRect
@@ -626,67 +619,5 @@
     [[NSWorkspace sharedWorkspace] activateFileViewerSelectingURLs:urls];
 }
 
-
-/*
--(BOOL)tableView:(TUITableView *)tableView canMoveRowAtIndexPath:(TUIFastIndexPath *)indexPath {
-    // return TRUE to enable row reordering by dragging; don't implement this method or return
-    // FALSE to disable
-    return TRUE;
-}
-
--(void)tableView:(TUITableView *)tableView moveRowAtIndexPath:(TUIFastIndexPath *)fromIndexPath toIndexPath:(TUIFastIndexPath *)toIndexPath {
-    PlaylistTrack *t = [_playlist trackAtIndex:[fromIndexPath row]];
-    [_playlist removeTrackAtIndex:[fromIndexPath row]];
-    [_playlist insertTrack:t atIndex:[toIndexPath row]];
-}
-
-- (BOOL)tableView:(TUITableView *)aTableView acceptDrop:(id <NSDraggingInfo>)info path:(TUIFastIndexPath *)path
-{
-    NSArray *filenames = [[info draggingPasteboard] propertyListForType:NSFilenamesPboardType];
-    int count=0;
-    for (NSString *s in [filenames reverseObjectEnumerator]) {
-        if([MusicController isSupportedAudioFile:s]) {
-            count++;
-            PlaylistTrack *t = [PlaylistTrack trackWithFilename:s inContext:_objectContext];
-            if([path row] == [_playlist numberOfTracks])
-                [_playlist addTrack:t];
-            else
-                [_playlist insertTrack:t atIndex:[path row]];
-        }
-    }
-
-    if(count == 0)
-        return NO;
-    else
-        return YES;
-}
-
-- (NSDragOperation)tableView:(TUITableView *)aTableView validateDrop:(id < NSDraggingInfo >)info proposedPath:(TUIFastIndexPath *)path
-{
-    NSArray *filenames = [[info draggingPasteboard] propertyListForType:NSFilenamesPboardType];
-    int count=0;
-    for (NSString *s in filenames)
-        if([MusicController isSupportedAudioFile:s])
-            count++;
-
-    if(count == 0) {
-        return NSDragOperationNone;
-    }
-    else {
-        return NSDragOperationCopy;
-    }
-}
-
-- (float)tableView:(TUITableView *)aTableView heightForDropGapAtIndexPath:(TUIFastIndexPath *)path drop:(id < NSDraggingInfo >)info
-{
-    NSArray *filenames = [[info draggingPasteboard] propertyListForType:NSFilenamesPboardType];
-    int count=0;
-    for (NSString *s in filenames)
-        if([MusicController isSupportedAudioFile:s])
-            count++;
-
-    return ((float)count)*25.0;
-}
-*/
 
 @end
