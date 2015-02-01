d4d553b22b675c8fa0d1f171ec2248108ebd1de1
Miles Wu <miles.wu@wu-home.co.uk>
2015-01-31 21:14:23 -0600

    Add MASPreferences license into credits.

--- scripts/generate-credits.sh
+++ scripts/generate-credits.sh
@@ -11,3 +11,4 @@ sed -i '' -e '/__LICENSE_OGG__/{' -e "r $SRCROOT/deps/ogg/COPYING" -e 'd' -e '}'
 sed -i '' -e '/__LICENSE_VORBIS__/{' -e "r $SRCROOT/deps/vorbis/COPYING" -e 'd' -e '}' "$CREDITS_HTML"
 sed -i '' -e '/__LICENSE_SPARKLE__/{' -e "r $SRCROOT/deps/sparkle/LICENSE" -e 'd' -e '}' "$CREDITS_HTML"
 sed -i '' -e '/__LICENSE_TAGLIB__/{' -e "r $SRCROOT/deps/taglib/COPYING.LGPL" -e 'd' -e '}' "$CREDITS_HTML"
+sed -i '' -e '/__LICENSE_MASPREFERENCES__/{' -e "r $SRCROOT/deps/MASPreferences/LICENSE.md" -e 'd' -e '}' "$CREDITS_HTML"
