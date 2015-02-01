872d98569ad99d8484cf2da44e6a95e01f895aba
Miles Wu <miles.wu@wu-home.co.uk>
2015-01-31 21:07:02 -0600

    Include license of dependencies in the credit generation script
    - Still missing MASPreferences
    - Moved script to its own bash file rather than in the Xcode project
      (easier to see diffs)

---
+++ scripts/generate-credits.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+CREDITS_HTML="$BUILT_PRODUCTS_DIR/$CONTENTS_FOLDER_PATH/Resources/en.lproj/Credits.html"
+
+sed -i '' -e '/__AUTHORS__/{' -e "r $SRCROOT/AUTHORS" -e 'd' -e '}' "$CREDITS_HTML"
+sed -i '' -e '/__LICENSE__/{' -e "r $SRCROOT/LICENSE" -e 'd' -e '}' "$CREDITS_HTML"
+sed -i '' -e '/__LICENSE_SPMEDIAKEYTAP__/{' -e "r $SRCROOT/deps/SPMediaKeyTap/LICENSE" -e 'd' -e '}' "$CREDITS_HTML"
+sed -i '' -e '/__LICENSE_COCOALUMBERJACK__/{' -e "r $SRCROOT/deps/cocoalumberjack/LICENSE.txt" -e 'd' -e '}' "$CREDITS_HTML"
+sed -i '' -e '/__LICENSE_FLAC__/{' -e "r $SRCROOT/deps/flac/COPYING.Xiph" -e 'd' -e '}' "$CREDITS_HTML"
+sed -i '' -e '/__LICENSE_OGG__/{' -e "r $SRCROOT/deps/ogg/COPYING" -e 'd' -e '}' "$CREDITS_HTML"
+sed -i '' -e '/__LICENSE_VORBIS__/{' -e "r $SRCROOT/deps/vorbis/COPYING" -e 'd' -e '}' "$CREDITS_HTML"
+sed -i '' -e '/__LICENSE_SPARKLE__/{' -e "r $SRCROOT/deps/sparkle/LICENSE" -e 'd' -e '}' "$CREDITS_HTML"
+sed -i '' -e '/__LICENSE_TAGLIB__/{' -e "r $SRCROOT/deps/taglib/COPYING.LGPL" -e 'd' -e '}' "$CREDITS_HTML"
