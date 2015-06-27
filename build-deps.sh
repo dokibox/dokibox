17fa9202641ce5425d86640929c7b19c25482a99
Miles Wu <miles.wu@wu-home.co.uk>
2015-06-26 22:39:13 -0500

    Add ffmpeg to deps/
    - Disable a lot of unnecessary stuff at ffmpeg compile time

--- build-deps.sh
+++ build-deps.sh
@@ -38,3 +38,20 @@ make -j2
 make install
 cd $PREFIX/lib
 install_name_tool -id @rpath/libtag.1.14.0.dylib libtag.1.14.0.dylib
+
+cd $DEPS/ffmpeg
+./configure --prefix=$PREFIX --disable-encoders --disable-hwaccels --disable-muxers --disable-devices --disable-filters --disable-programs --disable-protocols --enable-shared --disable-doc --disable-decoder=h264_vda
+make -j4
+make install
+cd $PREFIX/lib
+install_name_tool -id @rpath/libavformat.56.dylib libavformat.56.dylib
+install_name_tool -id @rpath/libavcodec.56.dylib libavcodec.56.dylib
+install_name_tool -id @rpath/libswresample.1.dylib libswresample.1.dylib
+install_name_tool -id @rpath/libavutil.54.dylib libavutil.54.dylib
+install_name_tool -change $PREFIX/lib/libavutil.54.dylib @rpath/libavutil.54.dylib libswresample.1.dylib
+install_name_tool -change $PREFIX/lib/libavutil.54.dylib @rpath/libavutil.54.dylib libavcodec.56.dylib
+install_name_tool -change $PREFIX/lib/libavutil.54.dylib @rpath/libavutil.54.dylib libavformat.56.dylib
+install_name_tool -change $PREFIX/lib/libswresample.1.dylib @rpath/libswresample.1.dylib libavcodec.56.dylib
+install_name_tool -change $PREFIX/lib/libswresample.1.dylib @rpath/libswresample.1.dylib libavformat.56.dylib
+install_name_tool -change $PREFIX/lib/libavcodec.56.dylib @rpath/libavcodec.56.dylib libavformat.56.dylib
+
